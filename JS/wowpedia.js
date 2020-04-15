7ef1b74983384fe8e21243075a191e30bffc83a5
Mateus Rigon <mateusrigon5@gmail.com>
2020-04-14 21:57:44 -0300

    Adicionado slideShow

--- JS/wowpedia.js
+++ JS/wowpedia.js
@@ -6,3 +6,49 @@ function cacador() {
 	$("#cacador").css({display: "block"});
 	$("#guerreiro").css({display: "none"});
 }
+
+var intervalo = 3000;
+var dot1 = document.getElementById('dot1');
+var dot2 = document.getElementById('dot2');
+var dot3 = document.getElementById('dot3');
+var dot4 = document.getElementById('dot4');
+
+function slide1(){
+	document.getElementById("imagem").src="Imagens/foto1.jpg";
+	dot1.classList.add("active");
+	dot4.classList.remove("active");
+	setTimeout("slide2()",intervalo);
+}
+
+function slide2(){
+	document.getElementById("imagem").src="Imagens/foto2.jpg";
+	dot2.classList.add("active");
+	dot1.classList.remove("active");
+	setTimeout("slide3()",intervalo);
+}
+
+function slide3(){
+	document.getElementById("imagem").src="Imagens/foto3.jpg";
+	dot3.classList.add("active");
+	dot2.classList.remove("active");
+	setTimeout("slide4()",intervalo);
+}
+
+function slide4(){
+	document.getElementById("imagem").src="Imagens/foto4.jpg";
+	dot4.classList.add("active");
+	dot3.classList.remove("active");
+	setTimeout("slide1()",intervalo);
+}
+
+// $(document).ready(function(){
+//   $("#menuEntrar").hover(function(){
+//     $(".user").css("display", "block");
+//     }, function(){
+//     $(".user").css("display", "none");
+//   });
+// });
+
+$('#navbarDropdown').on('click', function() {
+  $('.user').toggle();
+});
--- paginicial.php
+++ paginicial.php
@@ -19,41 +19,68 @@
 	<link rel="stylesheet" type="text/css" href="CSS/inicial.css">
 		<title>Pagina inicial</title>
 </head>
-<body>
+<body onload="slide1()">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 col-sm-12 col-6">
 				<nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-end">
-  					<h3>WOWPedia</h3>
+					<?php if (isset($_SESSION['login'])):?>
+				<div>
+			 	 <div id="usuario">
+			   		 <ul class="navbar-nav mr-auto">
+			     		 <li class="nav-item dropdown">
+			       		 <a class="nav-link dropdown-toggle" href="#" id="userlogin" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			        	  <?php echo $_SESSION['login']; ?>
+			       		 </a>
+			     	  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			       	   <form method="POST">
+							<input id="sair" type="submit" name="sair" value="sair">
+						</form>
+			       	   
+			      	  </div>
+			     	 </li>
+  					</div>
+				</div>
+				<?php endif ?>
+  					<h2>WOWPedia</h2>
 	
-  		   	 <form class="form-inline">
-     			 <input class="form-control mr-sm-2" type="search" placeholder="Procurar" aria-label="Search">
-   			 </form>
-
-   			 <div>
-			  <div id="navbarSupportedContent">
-			    <ul class="navbar-nav mr-auto">
-
-			      <li class="nav-item dropdown">
-			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			          Entrar
-			        </a>
-			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			          <a class="dropdown-item" href="#">Login</a>
-			          <a class="dropdown-item" href="#">Cadastrar</a>		  
-			        </div>
-			      </li>
-  				</div>
-			</div>
+  		   	<select id="inputState" class="form-control">
+        		<option selected>Buscar</option>
+        		<option onclick="guerreiro()">Guerreiro</option>
+        		<option>Caçador</option>
+        		<option>Sacerdote</option>
+        		<option>Xamã</option>
+        		<option>Bruxo</option>
+        		<option>Druida</option>
+        		<option>Paladino</option>
+        		<option>Ladino</option>
+        		<option>Cavaleiro da morte</option>
+        		<option>Mago</option>
+        		<option>Monge</option>
+        		<option>Caçador de demônios</option>
+     		 </select>
+     		 <!-- mostra entrar caso usuario não estiver logado -->
+     		 <?php if (empty($_SESSION['login'])) { ?>
+				<div>
+			 	 <div id="menuEntrar">
+			   		 <ul class="navbar-nav mr-auto">
+			     		 <li class="nav-item dropdown">
+			       		 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button">Entrar</a>
+			     	 </li>
+  					</div>
+				</div>
+			<?php
+			} ?>
 		</nav>
 	</div>
 </div>
 			<!-- imagem -->
-			<div class="row">
-				<div id="imagem" class="col-lg-12 col-sm-12 col-6">
-				<img class="img-fluid" src="Imagens/wow2.jpg">
+			<div class="row" id="slides">
+				<div class="mySlides col-lg-12 col-sm-12 col-6">
+				<img id="imagem" class="img-fluid">
 				</div>
 			</div>	
+
 			<!-- navbar secundaria -->
 			<div class="row">
 			<div class="col-lg-12 col-sm-12 col-6">
@@ -114,30 +141,21 @@
 			</div>
 		</div>
 
+<div id="dots">
+  <span id="dot1" class="dot"></span> 
+  <span id="dot2" class="dot"></span> 
+  <span id="dot3" class="dot"></span>
+  <span id="dot4" class="dot"></span> 
+</div>
+
+<div class="user">
+	<button onclick="location.href='index.php'" id="log">Login/Cadastrar</button>
+</div>
 
 
 
 
 
-
-
-
-				<?php 
-				if (isset($_SESSION['success'])) : 
-
-					echo $_SESSION['success'];
-					unset($_SESSION['success']);
-					
-				?>
-				<?php endif ?>
-				
-
-				<?php if (isset($_SESSION['login'])): ?>
-					<?php echo $_SESSION['login']; ?>
-					<form method="POST">
-							<input  type="submit" name="sair" value="sair">
-					</form>
-				<?php endif ?>
 
 							
 			
@@ -150,11 +168,9 @@
 
 
 
-
-
-	<script type="text/javascript" src="JS/wowpedia.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="JS/wowpedia.js"></script>
 </body>
 </html>
